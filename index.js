3d948ae3c3fd3ffb5d2b01c0aa9e116e6c63b634
Peace Kuria <ephraimekuria@gmail.com>
2025-09-20 17:10:19 +0300

    events

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 function addingEventListener() {
 }
+
+function addingEventListener() {
+  const input = document.getElementById("button");
+
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+
+  input.addEventListener("click", clickAlert);
+}
+
+// Make sure to call the function so it runs
+addingEventListener();
+
+input.addEventListener('click', clickAlert);   
+input.addEventListener('click', clickAlert()); 
+
+
